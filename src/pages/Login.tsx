056e72edc14a0284d845326c1b220b6e5aa7afe1
khaitbek <khaitbek2005@gmail.com>
2023-01-12 22:37:54 +0500

    fix routes error

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,7 +4,7 @@ import { SubmitHandler, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as zod from 'zod'
 import { Link } from "@mui/material";
-import { Link as RouterLink } from "react-router-dom";
+import { Link as RouterLink, useNavigate } from "react-router-dom";
 import "../lang/i18n";
 import { useTranslation } from "react-i18next";
 
@@ -18,12 +18,14 @@ function Login() {
         email: zod.string().email(),
         password: zod.string().min(8, "Your password should contain at least 8 characters")
     });
+    const navigate = useNavigate();
     const { register, formState: { errors, isValid }, handleSubmit } = useForm<InputTypes>({
         mode: "all",
         resolver: zodResolver(loginSchema)
     });
     const { t } = useTranslation()
-    const handleLogin: SubmitHandler<InputTypes> = data => console.log(data);
+        
+    const handleLogin: SubmitHandler<InputTypes> = data => navigate("/admin");
     return (
         <section className="login-section">
             <Container>
